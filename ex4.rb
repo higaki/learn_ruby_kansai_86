68547f3256cc0d4e43238bbd0dafce988aaef9e5
HIGAKI Masaru <mas.higa@gmail.com>
2019-05-06 14:08:18 +0900

    Attach harness to ex4

--- ex4.rb
+++ ex4.rb
@@ -1,10 +1,40 @@
-def ex
+def ex(trace)
+  trace << :yield
   result = yield
-  $stderr.puts :success
+  trace << :success
   result
+rescue
+  trace << :rescue
+  raise
 ensure
-  $stderr.puts :ensure
+  trace << :ensure
 end
 
-puts ex{1 + 1}
-puts ex{1 + "1"}
+require 'test/unit'
+class EnsureTest < Test::Unit::TestCase
+  def setup
+    @trace = []
+  end
+
+  def test_success
+    assert_equal(2, ex(@trace){1 + 1})
+    assert_equal([:yield, :success, :ensure], @trace)
+  end
+
+  def test_rescue
+    assert_raise(TypeError) do
+      ex(@trace){1 + "1"}
+    end
+    assert_equal([:yield, :rescue, :ensure], @trace)
+  end
+end
+
+# >> Loaded suite -
+# >> Started
+# >> ..
+# >> Finished in 0.000434 seconds.
+# >> -------------------------------------------------------------------------------
+# >> 2 tests, 4 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
+# >> 100% passed
+# >> -------------------------------------------------------------------------------
+# >> 4608.29 tests/s, 9216.59 assertions/s
